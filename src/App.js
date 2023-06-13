abcf7a5414f4ddc4fc6355ade715120c68b36c31
innovatium-mx <135089038+innovatium-mx@users.noreply.github.com>
2023-06-12 18:01:06 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { DataProvider } from "./Contexts/DataContext";
 
 function App() {
   //Link del api
-  const apiLink = "https://kvxrvsgw6c.execute-api.us-east-1.amazonaws.com";
+  const apiLink = "https://pxxfeffwyd.execute-api.us-east-2.amazonaws.com";
   //const apiLink = "http://localhost:3001";
 
   const { theme } = useContext(ThemeContext);
